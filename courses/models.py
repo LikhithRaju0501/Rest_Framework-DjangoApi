6e52b5f88a67f2027cff8c1873afefb701a43ebd
Likhith Raju <likhithgraju@gmail.com>
2020-02-03 19:43:39 +0530

    added url

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,6 @@ class Course(models.Model):
     name = models.CharField(max_length = 200)
     language = models.CharField(max_length = 100)
     price = models.CharField(max_length = 10)
-
+    
     def __str__(self):
         return self.name
--- courses/serializers.py
+++ courses/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Course
 
-class CourseSerializer(serializers.ModelSerializer):
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
-        fields = ('id','name','language','price')
+        fields = ('id','url','name','language','price')
